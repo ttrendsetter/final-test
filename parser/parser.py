c059eb9dcff41b313fbe57059b1f2f737e0f87b8
ttrendsetter <klo4kov.99@gmail.com>
2022-08-04 21:31:23 +0300

    some_changes

---
+++ parser/parser.py
@@ -0,0 +1,6 @@
+from django.http import request
+
+
+def parse():
+    for _ in range(100):
+        pass
--- parser/views.py
+++ parser/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from parser.parser import parse
+from django.contrib import messages
 # Create your views here.
 
+
 def parsing(request):
-    return HttpResponse('Parsing is started')
+    parse()
+    return HttpResponse('Parsing is finished')
