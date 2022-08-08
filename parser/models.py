e2bdc71920f319787fcbe8cad410b2a25fb4afdc
ttrendsetter <klo4kov.99@gmail.com>
2022-08-08 09:49:28 +0300

    changes in db

--- parser/models.py
+++ parser/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Towns(models.Model):
+    name = models.TextField(unique=True)
+    lon = models.FloatField(default=0)
+    lan = models.FloatField(default=0)
+class Weather(models.Model):
+    town = models.ForeignKey(
+        'Towns', on_delete=models.CASCADE)
+    weather = models.FloatField()
--- parser/parser.py
+++ parser/parser.py
@@ -1,6 +1,14 @@
-from django.http import request
+import sqlalchemy
+import requests
+import json
 
 
+
+#X-Yandex-API-Key: ba7a3712-6158-4598-87c0-df60309ab428
 def parse():
+    data = []
+
     for _ in range(100):
-        pass
+        response = requests.get('')
+        result = response.text
+
